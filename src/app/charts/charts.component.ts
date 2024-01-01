4012fb43692bffd5d4db03d713c871a262bb3f5b
Subins11 <122017529+Subins11@users.noreply.github.com>
2024-01-01 13:47:32 +0530

    chart

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -4,64 +4,71 @@ import * as d3 from 'd3';
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
-  styleUrls: ['./charts.component.scss']
+  styleUrls: ['./charts.component.scss'],
 })
 export class ChartsComponent implements OnInit {
   @ViewChild('chart') private chartContainer!: ElementRef;
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
     this.createLeaveChart();
   }
 
   private createLeaveChart(): void {
-    
+    //Datas
     const leavesData = [
       { employee: 'Present', leaves: 120 },
       { employee: 'Leave', leaves: 30 },
     ];
 
+    //SVG Container Setup
+
     const margin = { top: 20, right: 20, bottom: 30, left: 50 };
     const width = 250 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
-
-    const svg = d3.select(this.chartContainer.nativeElement)
+    const svg = d3
+      .select(this.chartContainer.nativeElement)
       .append('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-      const x: d3.ScaleBand<string> = d3.scaleBand()
+    //Scales
+    const x: d3.ScaleBand<string> = d3
+      .scaleBand()
       .range([0, width])
       .padding(0.1)
-      .domain(leavesData.map(d => d.employee));
-    
-      const leavesWithoutUndefined = leavesData.map(d => d.leaves).filter(value => value !== undefined) as number[];
-      const maxLeaves = d3.max(leavesWithoutUndefined) || 0;
-      const y = d3.scaleLinear()
-      .range([height, 0])
-      .domain([0, 150]);
+      .domain(leavesData.map((d) => d.employee));
 
-    svg.append('g')
+    const leavesWithoutUndefined = leavesData
+      .map((d) => d.leaves)
+      .filter((value) => value !== undefined) as number[];
+    const maxLeaves = d3.max(leavesWithoutUndefined) || 0;
+    const y = d3.scaleLinear().range([height, 0]).domain([0, 150]);
+
+    //Axes
+    svg
+      .append('g')
       .attr('transform', 'translate(0,' + height + ')')
       .call(d3.axisBottom(x));
 
-    svg.append('g')
-      .call(d3.axisLeft(y));
+    svg.append('g').call(d3.axisLeft(y));
 
-      svg.selectAll('.bar')
-  .data(leavesData)
-  .enter().append('rect')
-  .attr('class', 'bar')
-  .attr('x', d => x(d.employee) || 0)
-  .attr('width', x.bandwidth())
-  .attr('y', d => y(d.leaves || 0))
-  .attr('height', d => height - y(d.leaves || 0))
-  .attr('rx', 0)
-  .attr('ry', 0)
-  .attr('fill', d => d.employee === 'Present' ? '#bc5090' : '#ffa600');
+    //Bars
+    svg
+      .selectAll('.bar')
+      .data(leavesData)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => x(d.employee) || 0)
+      .attr('width', x.bandwidth())
+      .attr('y', (d) => y(d.leaves || 0))
+      .attr('height', (d) => height - y(d.leaves || 0))
+      .attr('rx', 0)
+      .attr('ry', 0)
+      .attr('fill', (d) => (d.employee === 'Present' ? '#bc5090' : '#ffa600'));
   }
 }
--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -1,73 +1,80 @@
-import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  NgZone,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
   selector: 'app-doughnut-chart',
   templateUrl: './doughnut-chart.component.html',
-  styleUrls: ['./doughnut-chart.component.scss']
+  styleUrls: ['./doughnut-chart.component.scss'],
 })
 export class DoughnutChartComponent implements OnInit {
-@ViewChild('doughnuChart', { static: true}) private chartContainer! : ElementRef
+  @ViewChild('doughnuChart', { static: true })
+  private chartContainer!: ElementRef;
 
-constructor (private zone: NgZone) {}
+  constructor(private zone: NgZone) {}
 
-ngOnInit(): void {
-  this.createDoughnutChart();
+  ngOnInit(): void {
+    this.createDoughnutChart();
+  }
+
+  createDoughnutChart() {
+    const data = [180, 30]; // Data
+    const width = 350;
+    const height = 350;
+    const radius = Math.min(width, height) / 2;
+    const color = d3.scaleOrdinal(['#0b9649', '#af0715c7']);
+
+    //SVG Container
+    const svg = d3
+      .select(this.chartContainer.nativeElement)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+    //Arc and Pie Functions
+    const arc = d3
+      .arc()
+      .innerRadius(radius - 100)
+      .outerRadius(radius);
+    const pie = d3.pie();
+
+    //Drawing Arcs
+    const arcs = svg
+      .selectAll('arc')
+      .data(pie(data))
+      .enter()
+      .append('g')
+      .attr('class', 'arc');
+
+    arcs
+      .append('path')
+      .attr('d', <any>arc)
+      .attr('fill', function (d: any, i: any) {
+        return color(i);
+      });
+
+    //Adding Labels
+    arcs
+      .append('text')
+      .attr('transform', function (d) {
+        const x = arc.centroid(<any>d)[0];
+        const y = arc.centroid(<any>d)[1];
+        return 'translate(' + x + ',' + y + ')';
+      })
+      .attr('dy', '0.35em')
+      .style('text-anchor', 'middle')
+      .style('fill', 'white')
+      .style('font-size', '12px')
+      .style('font-weight', '500')
+      .text(function (d, i) {
+        return i === 0 ? 'Active Employees' : 'Inactive Employees';
+      });
+  }
 }
-
-createDoughnutChart() {
-  const data = [180,30];
-  const width = 350;
-  const height = 350;
-  const radius = Math.min(width,height)/2;
-  const color = d3.scaleOrdinal(['#0b9649' ,'#af0715c7'  ]);
-
-  const svg = d3
-  .select(this.chartContainer.nativeElement)
-  .append('svg')
-  .attr('width',width)
-  .attr('height', height)
-  .append('g')
-  .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-  
-
-  const arc = 
-  d3.arc().innerRadius(radius - 100).outerRadius(radius);
-
-  const pie = d3.pie();
-
-  const arcs = svg
-  .selectAll('arc')
-  .data(pie(data))
-  .enter()
-  .append('g')
-  .attr('class','arc');
-
-  arcs
-  .append('path')
-  .attr('d', <any> arc)
-  .attr('fill', function (d: any, i: any)
-  {
-    return color(i);
-  })
-
-arcs
-.append('text')
-.attr('transform', function (d) {
-  const x = arc.centroid( <any>d)[0];
-  const y = arc.centroid(<any>d)[1];
-  return 'translate(' + x + ',' + y + ')';
-})
-.attr('dy', '0.35em')
-.style('text-anchor', 'middle')
-.style('fill', 'white')
-.style('font-size', '12px')
-.style('font-weight', '500')
-.text(function (d, i) {
-  return i === 0 ? 'Active Employees' : 'Inactive Employees';
-})
-
-}
-}
-
-
